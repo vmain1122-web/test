ac61d0f2ff3efda1499a140f7cc655b17c2be161
ぴぴ <vmain1122@gmail.com>
2026-05-26 10:08:18 +0900

    Add files via upload

---
+++ DrinkRequest.java
@@ -0,0 +1,10 @@
+public class DrinkRequest {
+    private String castId;
+    private List<String> ingredients;
+
+    // Getters and Setters
+    public String getCastId() { return castId; }
+    public void setCastId(String castId) { this.castId = castId; }
+    public List<String> getIngredients() { return ingredients; }
+    public void setIngredients(List<String> ingredients) { this.ingredients = ingredients; }
+}
